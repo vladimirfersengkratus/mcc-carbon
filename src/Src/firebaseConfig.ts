2b848f3376583aeb9413e40477358bb456d24e77
Vladimir Ferreira Silva <eng.vladimirferreira@gmail.com>
2025-07-10 11:30:15 -0300

    Create firebaseConfig.ts

---
+++ src/Src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWdDqI7Uk8uKBY80Oy-vtsqpLFU00m9bM",
+  authDomain: "mcc-carbon.firebaseapp.com",
+  projectId: "mcc-carbon",
+  storageBucket: "mcc-carbon.firebasestorage.app",
+  messagingSenderId: "260261175727",
+  appId: "1:260261175727:web:5cbec6c6b19b0a60accf5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
